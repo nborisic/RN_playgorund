b0956f003545118b642eb43587d8cb8efa5eda9a
Nikola Borisic <borisic.nikola@yahoo.com>
2018-01-08 14:29:35 +0100

    initital commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Dobar dan dobri radni ljudi, i Marko.
+          Ovde se igramo sa RN. YAY!!1
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
